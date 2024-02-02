062a2c22d2db0ed529a3000d2b402b0ea7f0f7e8
Devin Maahs <ddmaahs@icloud.com>
2024-02-02 11:48:08 -0600

    LaserCan support

--- src/main/java/frc/lib/sensors/BeamBreakFactory.java
+++ src/main/java/frc/lib/sensors/BeamBreakFactory.java
@@ -1,5 +1,11 @@
 package frc.lib.sensors;
 
+import au.grapplerobotics.ConfigurationFailedException;
+import au.grapplerobotics.LaserCan;
+import au.grapplerobotics.LaserCan.Measurement;
+import au.grapplerobotics.LaserCan.TimingBudget;
+
+
 public final class BeamBreakFactory {
     
     /**
@@ -44,5 +50,49 @@ public final class BeamBreakFactory {
             throw new IllegalArgumentException("max must be greater than 0");
         }
         return create(tof, 0, max);
+    }
+
+    public static BeamBreak createLazerCAN(int canID, double distThreshold){
+
+        @SuppressWarnings("resource")
+        LaserCan sensor = new LaserCan(canID);
+
+        try{
+            //Setting max distance lower if possible for more accurate measurements
+            if(distThreshold < 1300){
+                sensor.setRangingMode(LaserCan.RangingMode.SHORT);
+            }else{
+                sensor.setRangingMode(LaserCan.RangingMode.LONG);
+            }
+
+            //setting to fastest refresh rate
+            sensor.setTimingBudget(TimingBudget.TIMING_BUDGET_20MS);
+
+            //setting to narrow fov
+            sensor.setRegionOfInterest(new LaserCan.RegionOfInterest(8,8,6,6));//TODO probably tune this and also make sure it is centered
+        }catch(ConfigurationFailedException e){
+            System.err.println("LaserCan, more like LaserCan't");
+            e.printStackTrace();
+        }
+
+        BeamBreak bb = new BeamBreak() {
+            private final LaserCan _sensor = sensor;
+            /**The minimum distance in which the beam is not broken in mm*/
+            private final double _distThreshold = distThreshold;
+
+
+            @Override
+            public boolean isBroken(){
+                Measurement measurement = _sensor.getMeasurement();
+                //if the measurement is funny, we don't consider it. I don't feel like throwing an exception or logging rn
+                if(measurement.status != LaserCan.LASERCAN_STATUS_VALID_MEASUREMENT){
+                    return false;
+                }
+
+                return measurement.distance_mm < _distThreshold;
+            }
+        };
+
+        return bb;
     }
 }
