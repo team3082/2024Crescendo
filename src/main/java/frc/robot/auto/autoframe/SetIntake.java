5d61cea74607f3686509730bfc402f00f030a380
Kade J Rabe <kaderabe04@gmail.com>
2024-02-08 08:59:08 -0600

    one small change to the autoframe SetIntake

--- src/main/java/frc/robot/auto/autoframe/SetIntake.java
+++ src/main/java/frc/robot/auto/autoframe/SetIntake.java
@@ -10,12 +10,14 @@ public class SetIntake extends Autoframe {
     @Override
     public void start() {
         Intake.setIntakePosition(Intake.IntakePosition.GROUND);
+        Intake.setIntakeVelocity(1);
     }
 
     @Override
     public void update() {        
         if (Intake.pieceGrabbed()) {
             Intake.setIntakePosition(Intake.IntakePosition.INROBOT);
+            Intake.setIntakeVelocity(0);
             this.done = true;
         }
     }
